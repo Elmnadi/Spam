32dbece403d67d5db107636863f63145e7bd0d81
Elmnadi <49082229+Elmnadi@users.noreply.github.com>
2019-03-30 05:18:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,6 @@ const client2 = new Discord.Client();
 const client3 = new Discord.Client();
 const client4 = new Discord.Client();
 const client5 = new Discord.Client();
-const client6 = new Discord.Client();
 
 
 
@@ -112,24 +111,6 @@ client5.on('message', message => {
     }
 });
 
-client6.on('message', message => {
-    if(message.content === '-راتب'){
-        message.channel.send('#daily')
-    }
-});
-
-client6.on('message', message => {
-    if(message.content === '-مبلغ'){
-        message.channel.send('#credits')
-    }
-});
-
-client6.on('message', message => {
-    if(message.content === '-ريب'){
-        message.channel.send('#credits')
-    }
-});
-
 
 client.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
 if (message.content === '!spam') {
@@ -201,26 +182,9 @@ if (message.content === '!spam') {
       }
 });
 
-client6.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
-if (message.content === '!spam') {
-      let count = 0;
-      let ecount = 0;
-      for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
-          .then(m => {
-            count++;
-          })
-          
-        }
-      }
-});
-
-
-
 
 client.login(process.env.TOKEN);// لا تغير فيها شيء
 client2.login(process.env.TOKEN2);// لا تغير فيها شيء
 client3.login(process.env.TOKEN3);// لا تغير فيها شيء
 client4.login(process.env.TOKEN4);// لا تغير فيها شيء
 client5.login(process.env.TOKEN5);// لا تغير فيها شيء
-client6.login(process.env.TOKEN6);// لا تغير فيها شيء
